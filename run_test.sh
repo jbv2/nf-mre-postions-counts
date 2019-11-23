c6762cbe778e22461759e23c6ab021d21561c5f8
judith <judith.vballesteros@gmail.com>
2019-11-23 13:26:47 -0600

    NF: restoring config

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,2 @@
 nextflow run mre_positions_counts.nf --input_dir test/data -resume -with-report test/results/nf_report.html \
-&& echo -e "======\n VCF summarizer: Basic pipeline TEST SUCCESSFUL \n======"
+&& echo -e "======\n nf-mre_positions_counts: Basic pipeline TEST SUCCESSFUL \n======"
